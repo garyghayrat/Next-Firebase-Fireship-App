20c0db6b5389e39522752484032a45c2e0ab7bd8
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-07-29 14:57:06 -0400

    Complete app section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Loader from "../components/Loader";
+import toast from "react-hot-toast";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Loader show />
+      <Link
+        prefetch={false}
+        href={{
+          pathname: "/[username]",
+          query: { username: "jeffd23" },
+        }}
+      >
+        <a>Jeff's profile</a>
+      </Link>
+      <div>
+        <button onClick={() => toast.success("hello toasty!")}>
+          Toast me!
+        </button>
+      </div>
+    </div>
+  );
 }
